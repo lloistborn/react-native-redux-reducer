59ad59df100f79942d04e46407cb09352b467aa2
lloisborn <faisalmorensya@gmail.com>
2017-11-12 13:28:35 +0700

    adding reducers, common components

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('reduxboilerplate', () => App);
+AppRegistry.registerComponent('reduxboilerplate', () => {
+  return App;
+});

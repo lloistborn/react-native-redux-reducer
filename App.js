1cef4c4cb6a5e96d2baedb401d5ee3cf3d503df6
lloisborn <faisalmorensya@gmail.com>
2017-11-12 15:41:58 +0700

    redux mapStateToProps

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './src/reducers';
-
 import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,15 @@
 import { connect } from 'react-redux';
+import { Component } from 'react';
 
-const LibraryList = () => {
-  return {
-    render: () => {
-      return 0;
-    },
-  };
+class LibraryList extends Component {
+  render() {
+    console.log(this.props);
+    return;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { libraries: state.libraries };
 };
 
-export default connect()(LibraryList);
+export default connect(mapStateToProps)(LibraryList);
